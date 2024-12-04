0121438b39de352a42608b2bfed3a25bd85f1ab7
JEEVANRAWAL <rawaljeevan123@gmail.com>
2024-12-04 15:25:10 +0545

    shadcn ui configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        "2xs": "350px",
-        xs: "450px", // Adding a new breakpoint for extra small screens
-        "3xl": "1600px", // Adding a breakpoint for very large screens
-      },
-    },
+  	extend: {
+  		screens: {
+  			'2xs': '350px',
+  			xs: '450px',
+  			'3xl': '1600px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
