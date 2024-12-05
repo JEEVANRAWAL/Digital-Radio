6cc3136431eebaab039fd088e187fdfd77fe97ce
JEEVANRAWAL <rawaljeevan123@gmail.com>
2024-12-05 19:00:53 +0545

    fixed: tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		screens: {
-  			'2xs': '350px',
-  			xs: '450px',
-  			'3xl': '1600px'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      screens: {
+        "2xs": "350px",
+        xs: "450px",
+        "3xl": "1600px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate], // Use the imported plugin
 };
